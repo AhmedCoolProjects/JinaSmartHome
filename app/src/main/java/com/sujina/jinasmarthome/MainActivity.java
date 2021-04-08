895df5cf96490215e1c38a794c4eef8c551fdc41
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-04-08 09:57:30 +0100

    finished first design for the bedroom layout.

--- app/src/main/java/com/sujina/jinasmarthome/MainActivity.java
+++ app/src/main/java/com/sujina/jinasmarthome/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    Button btnBedroom;
     String[] s1 = {"Morning","I'm Out","Back Home"};
 
     int[] images = {R.drawable.ic_sunrise_icon,R.drawable.ic_out_icon,R.drawable.ic_back_icon};
@@ -18,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recy_home);
         recyclerView = findViewById(R.id.recy_home);
+        btnBedroom = findViewById(R.id.btn_bedroom);
         MyAdapter myAdapter = new MyAdapter(this,s1,images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        /*navigation*/
+        btnBedroom.setOnClickListener(v->{
+            Intent intent = new Intent(this,Bedroom.class);
+            startActivity(intent);
+        });
     }
 }
