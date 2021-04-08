20773c69bc5fd703c4ff92d8a8842c8c4a21964d
Ahmed BARGADY <bargadyahmed2000@gmail.com.com>
2021-04-08 09:20:32 +0100

    finished first design for the home layout.

--- app/src/main/java/com/sujina/jinasmarthome/MainActivity.java
+++ app/src/main/java/com/sujina/jinasmarthome/MainActivity.java
@@ -1,14 +1,25 @@
 package com.sujina.jinasmarthome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    String[] s1 = {"Morning","I'm Out","Back Home"};
+
+    int[] images = {R.drawable.ic_sunrise_icon,R.drawable.ic_out_icon,R.drawable.ic_back_icon};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recy_home);
+        recyclerView = findViewById(R.id.recy_home);
+        MyAdapter myAdapter = new MyAdapter(this,s1,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
     }
 }
